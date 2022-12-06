9f271ba44fb24e2d4ee6b282cf33facbfbf52562
DthYb <osiris.fd7@gmail.com>
2022-12-06 14:51:38 +0100

    Page de login

--- app/src/main/java/com/example/all4sport/MainActivity.java
+++ app/src/main/java/com/example/all4sport/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.all4sport;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn1;
+    private TextView titre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.btn1 = (Button) findViewById(R.id.btn1);
+        this.titre = (TextView) findViewById(R.id.titre);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                titre.setText("Vous avez cliqué sur le Bouton !!!");
+            }
+        });
     }
+
 }
